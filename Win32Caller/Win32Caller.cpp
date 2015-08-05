1930d4cf677d37beb3a9e7f76c78510ba0582766
Jacob L Anawalt <jlanawalt@gmail.com>
2015-08-05 11:34:47 -0600

    Create a application to call this DLL
    Step 4 by Jesse Jiang: "Create a application to call this DLL, I suggest
    you to create a console application with MFC support"

---
+++ Win32Caller/Win32Caller.cpp
@@ -0,0 +1,46 @@
+// Win32Caller.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Win32Caller.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// The one and only application object
+
+CWinApp theApp;
+
+using namespace std;
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	int nRetCode = 0;
+
+	HMODULE hModule = ::GetModuleHandle(NULL);
+
+	if (hModule != NULL)
+	{
+		// initialize MFC and print and error on failure
+		if (!AfxWinInit(hModule, NULL, ::GetCommandLine(), 0))
+		{
+			// TODO: change error code to suit your needs
+			_tprintf(_T("Fatal Error: MFC initialization failed\n"));
+			nRetCode = 1;
+		}
+		else
+		{
+			// TODO: code your application's behavior here.
+		}
+	}
+	else
+	{
+		// TODO: change error code to suit your needs
+		_tprintf(_T("Fatal Error: GetModuleHandle failed\n"));
+		nRetCode = 1;
+	}
+
+	return nRetCode;
+}
